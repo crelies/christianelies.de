67cfaebc6c03be08d4c76bda3b6e35e5383b2d36
Chris <chris.elies13@gmail.com>
2019-01-16 23:58:39 +0100

    chore(): updated vapor and leaf

--- Package.swift
+++ Package.swift
@@ -5,10 +5,10 @@ let package = Package(
     name: "christianelies",
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.1.1"),
 
         // 🍃 An expressive, performant, and extensible templating language built for Swift.
-        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.2"),
     ],
     targets: [
         .target(name: "App", dependencies: ["Leaf", "Vapor"]),
